70eb011376412af6c7b2f8ff2b4a296666131f72
Leandro Romero <romerodisind@gmail.com>
2023-05-28 23:28:48 -0300

    actualizacion cli

--- src/app/auth/guards/admin.guard.ts
+++ src/app/auth/guards/admin.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable, map } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AdminGuard implements CanActivate {
+export class AdminGuard  {
   constructor(private authService: AuthService) {}
 
   canActivate(
